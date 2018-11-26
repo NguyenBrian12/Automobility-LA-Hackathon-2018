14e6a3ccf986e2430d8721842ee6a9c9f2ee73c4
frazermg <mgriffiths29@gmail.com>
2018-11-26 10:24:27 -0800

    back button working

--- GmAppReact/src/service-worker.js
+++ GmAppReact/src/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts("/precache-manifest.fb827b33fb2933208ea5122b9c66eafe.js");
+importScripts(
+  "/precache-manifest.2cf52f8fe0ef975460451b0c63e93bbf.js"
+);
 
 workbox.clientsClaim();
 
@@ -29,5 +29,6 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
-  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

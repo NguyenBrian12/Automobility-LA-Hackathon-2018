fa390e0e729795293cd09baa800c6d61f3059f0a
Emily Sanchez <myejsg@yahoo.com>
2018-11-25 21:13:40 -0800

    1

---
+++ BoostedCampers/BoostedCampers/Models/Locator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoostedCampers.Models
+{
+    public class Locator
+    {
+        public Header header { get; set; }
+        public SearchAttrList searchAttrList { get; set; }
+        public string responseAttrList { get; set; }
+        public SearchOptions searchOptions { get; set; }
+
+    }
+}
